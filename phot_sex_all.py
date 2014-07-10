9358f11c48c750f19a3015fb14db312b657faf8a
Zach Pace <zpace@itsd-summer10.stsci.edu>
2014-07-10 16:02:05 -0400

    Conversion to uJy
    initial conversion to uJy. still some clean up to do, esp with errors
    and Y1 photometry. stay tuned.

--- phot_sex_all.py
+++ phot_sex_all.py
@@ -75,28 +75,15 @@ def obj_phot(x, y, maxdist = 3):
 		
 	#ds9.set('exit')
 	return COG, COG_e
+
+def uJ(data, band):
+	'''
+	takes a single flux measurement and a band string, and uses the ZP dict to output a
+	flux in uJ (microJanskys)
+	'''
+	return data * 10.**(-0.4 * (ZPs[band] - 23.9))
 	
-def SED(data, error, max_aper):
-	'''
-	input a COG dictionary (data) with all bands (such as from obj_phot), along with a 
-	maximum aperture (diameter, in arcseconds), normalize to that aperture, and return an 
-	SED at that aperture
-	'''
-	vals = []
-	errs = []
-	apers = np.array([2,3,4,6,8,10,14,20,28,40,60,80,100,160])*0.06
-	for key in data.keys():
-		norm_val = np.interp(max_aper, apers, data[key])
-		vals.append(norm_val)
-		norm_err = np.interp(max_aper, apers, error[key])
-		errs.append(norm_err)
-		#plt.errorbar(bands[key], norm_val, yerr = norm_err, marker = 'x', color = 'k')
-	#plt.xlabel('Wavelength (nm)')
-	#plt.ylabel('Log10 of Flux')
-	#plt.title('SED at aperture d = ' + str(max_aper) + '"')
-	#plt.yscale('log', basex = 10)
-	#plt.show()
-	return np.asarray(vals), np.asarray(errs)
+uJ_v = np.vectorize(uJ)
 	
 def COG_plot(COG, COG_e, max_aper):
 	#now for the heck of it, make a COG in all bands simultaneously
@@ -106,13 +93,30 @@ def COG_plot(COG, COG_e, max_aper):
 		norm_err = np.interp(max_aper, apers, COG_e[key])
 		#print COG[key]/norm_val
 		plt.errorbar(apers, np.asarray(COG[key])/norm_val, marker = style[key][1], color = style[key][0], label = key)
-
 	plt.legend(loc = 'best')
 	plt.xlim([0., max_aper])
 	plt.ylim([0., 1.])
 	plt.xlabel('Aperture Diameter (arcsec)')
 	plt.ylabel('Fraction of flux')
 	plt.show()
+	
+def SED(data, error, max_aper):
+	'''
+	input a COG dictionary (data) with all bands (such as from obj_phot), along with a 
+	maximum aperture (diameter, in arcseconds), normalize to that aperture, and return an 
+	SED at that aperture
+	'''
+	vals = []
+	errs = []
+	keys = []
+	apers = np.array([2,3,4,6,8,10,14,20,28,40,60,80,100,160])*0.06
+	for key in data.keys():
+		interp_val = np.interp(max_aper, apers, data[key])
+		vals.append(interp_val)
+		interp_err = np.interp(max_aper, apers, error[key])
+		errs.append(interp_err)
+		keys.append(key)
+	return np.asarray(vals), np.asarray(errs), keys
 
 #Generate COG dicts for a star
 coords = np.array([ [2284, 2204], [3380, 2051], [2778, 1481], [2810, 3210], [3043, 1691] ])
@@ -121,47 +125,56 @@ bands = {'105': 1055.2, '125': 1248.6, '140': 1392.3, '160': 1536.9, '435': 429.
 #check the center of the K-band
 bands_wid = {'105': 265/2., '125': 284.5/2., '140': 384/2., '160': 268.3/2., '435': 103.8/2., '606': 234.2/2., '814': 251.1/2., 'K': 400./2.}
 style = {'105': ['r', 'x'], '125': ['g', 'x'], '140': ['b', 'x'], '160': ['k', 'x'], '435': ['r', 'o'], '606': ['g', 'o'], '814': ['b', 'o'], 'K': ['k', 'o']}
+ZPs = {'105':26.2687, '125':26.25, '140':26.46, '160':25.96, '435':25.65777, '606':26.486, '814':25.937, 'K':26.0}
 
 #ADJUST THINGS HERE
-i = 0
-max_aper = 1.2
+i = 1 #which star?
+max_aper = 1.6
 # =====
 
 #get data for a star
 COG, COG_e = obj_phot(coords[i, 0], coords[i, 1])
-vals, errs = SED(COG, COG_e, max_aper)
-COG_plot(COG, COG_e, max_aper)
+#vals, errs, keys = SED(COG, COG_e, max_aper)
+#COG_plot(COG, COG_e, max_aper)
 
 #'''
 #now make an SED with a bunch of different apertures for a star	
-vals, errs = SED(COG, COG_e, max_aper)
-for a in [.2, .4, .6, .9, 1.2]:
-	vals, errs = SED(COG, COG_e, a)
-	plt.errorbar(np.asarray(bands.values())/1000., vals, xerr = np.asarray(bands_wid.values())/1000., yerr = errs, marker = 'x', linestyle = 'None', label = str(a) + '"')
-plt.xscale('log')
-plt.yscale('log')
+for a in [.12, .4]:
+	print 'aper:', a
+	vals, errs, keys = SED(COG, COG_e, a)
+	l = np.asarray([bands[key] for key in keys])
+	lw = np.asarray([bands_wid[key] for key in keys])
+	print uJ_v(vals, keys)
+	print uJ_v(errs, keys)
+	#print l, lw, keys
+	plt.errorbar(l/1000., uJ_v(vals, keys), xerr = lw/1000., yerr = errs, marker = 'x', linestyle = 'None', label = str(np.round(a, decimals = 1)) + '"')
+#plt.xscale('log')
+#plt.yscale('log')
 plt.xlabel('wavelength (microns)')
-plt.ylabel('flux')
+plt.ylabel('flux (uJ)')
 plt.title('Star SED for several apertures')
-plt.legend(loc = 'best', title = 'Apertures')
+plt.legend(loc = 'best', title = 'ap. diam.')
 plt.show()
 #'''
 
+'''
 #get data for Y1
 COG, COG_e = obj_phot(Y1_coords[0], Y1_coords[1])
 vals, errs = SED(COG, COG_e, max_aper)
 #COG_plot(COG, COG_e, max_aper) #galaxy COG isn't very instructive
-#'''
+
 #now do the same for Y1
-for a in np.logspace(0., 1.25, num = 4):
-	vals, errs = SED(COG, COG_e, a)
-	plt.errorbar(np.asarray(bands.values())/1000., vals, xerr = np.asarray(bands_wid.values())/1000., yerr = errs, marker = 'x', linestyle = 'None', label = str(a) + '"')
-plt.xscale('log')
-plt.yscale('log')
+for a in [2.]:
+	vals, errs, keys = SED(COG, COG_e, a)
+	#print 'aperture:', a
+	#print vals, errs
+	plt.errorbar(np.asarray(bands.values())/1000., vals, xerr = np.asarray(bands_wid.values())/1000., yerr = errs, marker = 'x', linestyle = 'None', label = str(np.round(a, decimals = 1)) + '"')
+#plt.xscale('log')
+#plt.yscale('log')
 plt.xlabel('wavelength (microns)')
 plt.ylabel('flux')
 plt.title('Galaxy SED for several apertures')
-plt.legend(loc = 'best', title = 'Apertures')
+plt.legend(loc = 'best', title = 'Ap. diam.')
 plt.show()
-#'''
+'''
 #We're getting detection in all bands (!!!)
